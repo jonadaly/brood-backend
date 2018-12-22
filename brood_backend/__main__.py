7aa79bde59868a900e5a56efef6e02c1a0f280ba
Jon Daly <jonathan.daly@draysontechnologies.com>
2018-12-22 13:43:01 +0000

    Updated port

--- brood_backend/__main__.py
+++ brood_backend/__main__.py
@@ -2,8 +2,8 @@ import os
 import waitress
 from brood_backend.app import create_app
 
-SERVER_PORT = os.getenv("SERVER_PORT", 8080)
+PORT = os.getenv("PORT", 8080)
 
 if __name__ == "__main__":
     app = create_app()
-    waitress.serve(app, host="0.0.0.0", port=SERVER_PORT)
+    waitress.serve(app, host="0.0.0.0", port=PORT)
--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SERVER_PORT=${1-5000}
+export PORT=${1-5000}
 export FLASK_APP=brood_backend/autoapp.py
 export DATABASE_URL=postgres://brood:brood@localhost:5432/brood
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
